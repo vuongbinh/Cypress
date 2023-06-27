7d6aa4c236eca15efe7a4cd4591a7eec98264f22
Vuong Binh <binh.vuonghao@gmail.com>
2023-06-27 23:28:11 +0700

    Cypress custom commands

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,20 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+/**
+ * @memberOf cy
+ * @method login
+ * */
+Cypress.Commands.add('login', (usr, pwd) => {
+    cy.request({
+        url: 'https://api.demoblaze.com/login',
+        method: 'POST',
+        headers: {
+            ContentType: 'application/json'
+        },
+        body: {
+            username: usr, password: btoa(pwd)
+        }
+    }).then(res => {
+        const authToken = res.body.split('Auth_token: ')[1];
+        cy.log(authToken);
+        cy.setCookie('tokenp_', authToken)
+    })
+})
